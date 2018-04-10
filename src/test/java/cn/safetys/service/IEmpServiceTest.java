edae2ea19fdcc3a999aacc308d53771ac88c217d
chedeqing1001 <251552563@qq.com>
2018-04-10 21:03:50 +0800

    实现单元测试代码

--- src/test/java/cn/safetys/service/IEmpServiceTest.java
+++ src/test/java/cn/safetys/service/IEmpServiceTest.java
@@ -1,5 +1,7 @@
 package cn.safetys.service;
 
+import cn.safetys.vo.Emp;
+import junit.framework.TestCase;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,14 +12,19 @@ import static org.junit.Assert.*;
 public class IEmpServiceTest {
     @Test
     public void insert() throws Exception {
+        TestCase.assertTrue(true);
     }
 
     @Test
     public void update() throws Exception {
+        Emp vo = new Emp();
+        vo.setEname("lisi");
+        TestCase.assertNotNull(vo);
     }
 
     @Test
     public void delete() throws Exception {
+        TestCase.assertTrue(false);
     }
 
 }
